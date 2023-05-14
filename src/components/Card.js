148f8cd967352c0b69f147031efefb31daa5443b
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-14 20:23:20 +0530

    Add the shuffling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,6 @@
 import alienImages from "../images/imageList";
 
 const Card = (props) => {
-  console.log(props.alien);
   return (
     <div className="card">
       <img src={alienImages[props.alien]} alt={props.alien} />
--- src/components/CardSet.js
+++ src/components/CardSet.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Card from "./Card";
 
 const alienList = [
@@ -19,10 +20,41 @@ const alienList = [
   "XLR8",
 ];
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
 const CardSet = () => {
+  const [state, setState] = useState(0);
+
+  const rerender = () => {
+    setState(state + 1);
+  };
+
   const cards = [];
-  alienList.forEach((alien) => cards.push(<Card alien={alien} />));
-  return <div className="card-list">{cards}</div>;
+  shuffle(alienList);
+  alienList.forEach((alien) =>
+    cards.push(<Card alien={alien} key={alien + Math.random()} />)
+  );
+  return (
+    <>
+      <div className="card-list">{cards}</div>
+      <button onClick={rerender}>Re-Render</button>
+    </>
+  );
 };
 
 export default CardSet;
