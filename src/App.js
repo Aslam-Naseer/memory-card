aeb76aa09aec4a12e1aa0bb397c04e73b0b40432
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-14 15:12:12 +0530

    Add necessary component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "./styles.css";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <div>Hello</div>
+      <Header />
     </div>
   );
 }
